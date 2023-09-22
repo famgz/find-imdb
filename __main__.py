763b8717ee78e2843a1b3a4e757f774698faf9dc
famgz <famgz@proton.me>
2023-09-21 21:46:41 -0300

    started implementing module structure

--- __main__.py
+++ __main__.py
@@ -1,6 +1,13 @@
 import sys
 import main
 
+if __name__ == "__main__":
+    usage = ""
 
-
-args = sys.argv
+    # TODO implement args version to function as module receiving user input
+    args = sys.argv
+    print(args)
+    while len(args) < 2:
+        print("Invalid parameters")
+        print(usage)
+    main(*args[1:])
