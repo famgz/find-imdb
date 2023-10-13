eaa002a6108be60232ac1d606c1a2af49ccb6fe2
famgz <famgz@proton.me>
2023-10-12 22:01:49 -0300

    several search and string improvements

--- src/find_imdb/__main__.py
+++ src/find_imdb/__main__.py
@@ -8,8 +8,6 @@ if __package__ is None:
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
-import main
-
 if __name__ == "__main__":
     usage = ""
 
--- src/find_imdb/main.py
+++ src/find_imdb/main.py
@@ -1,94 +1,131 @@
 from difflib import SequenceMatcher
-from urllib.error import HTTPError
+from famgz_utils import print, timeit
 from imdb import Cinemagoer as IMDb
 from time import sleep
-from rich import print
-from famgz_utils import clean_name, timeit
+from string import ascii_lowercase, digits
+from unidecode import unidecode
+from urllib.error import HTTPError
 
 TRIES = 1
 INTERVAL = 0.1
-SEARCH_TOLERANCE = 5
+DEBUG = False
+DIRECTORS_SEARCH_TOLERANCE = 2
+MOVIES_SEARCH_TOLERANCE = 5
 SIMILARITY_THRESHOLD = 0.9
+allowed_chars = ascii_lowercase + digits + ' '
+removed_title_words = ['the', 'that']
+
+
+def dprint(*x):
+    if not DEBUG:
+        return
+    for i in x:
+        print(f'[bright_black]{i}', end=' ')
+    print()
 
 
 def is_similar_string(a, b):
     similarity = SequenceMatcher(None, a, b).ratio()
-    return similarity >= SIMILARITY_TRESHOLD
+    return similarity >= SIMILARITY_THRESHOLD
 
 
-def normalize_strings(names):
-    # normalize movie titles
-    if isinstance(names, str):
-        return clean_name(names, dot=False).lower()
-    # normalize people names
-    elif isinstance(names, list):
-        first_names = sorted([normalize_strings(str(x)).split()[0] for x in names if x])  # reduces all clean names to its first name
-        first_names.sort()
-        return first_names
+def clean_string(s):
+    s = unidecode(s)
+    s = s.lower()
+    s = ''.join([x for x in s if x in allowed_chars])
+    while '  ' in s:
+        s = s.replace('  ', ' ')
+    return s
+
+
+def clean_title(s):
+    s = clean_string(s)
+    return ' '.join([x for x in s.split() if x not in removed_title_words])
+
+
+def clean_names(names):
+    # normalize people names and reduce to first name
+    return sorted([clean_string(str(x)).split()[0] for x in names if x])  # reduces all clean names to its first name
 
 
 @timeit
-def main(title_eng=None, original_title=None, directors=None, data:dict=None):
+def main(*titles, directors=None, data: dict = None):
 
     def search_id_by_director():
-        # Search by director name and compares by title_eng/original_title unidecode lower
-        for director in directors_list:
-            people = ia.search_person(director)
+        # Search by director name and compare titles
+        dprint('\n>> search_id_by_director')
+        for director in directors:
+            dprint('director=', director)
+            people = ia.search_person(director)[:DIRECTORS_SEARCH_TOLERANCE]
             for p in people:
+                dprint('person=', p)
                 person = ia.get_person(p.getID())
-                filmography = person.get('filmography', {}).get('director')
+                filmography = person.get('filmography', {}).get('director')  # it only returns display titles
                 if filmography:
                     for f in filmography:
-                        film = normalize_strings(str(f))
-                        if film in [normalize_strings(x) for x in titles]:  # match title
+                        dprint('film=', f)
+                        film = clean_title(str(f))
+                        dprint('cleaned film=', film)
+                        if film in cleaned_titles:  # match title
                             ID = f.getID()
                             return 'tt' + ID
                 sleep(INTERVAL)
         return None
 
     def search_id_by_title(movies):
-        # Search by title_eng/original_title and compares by director's first name unidecode lower
-        if movies:
-            for m in movies[:SEARCH_TOLERANCE]:
-                ID = m.getID()
-                movie = ia.get_movie(ID)
-                imdb_directors = movie.get('directors')
-                if imdb_directors:
-                    imdb_directors = normalize_strings(imdb_directors)
-                    matches = [x for x in directors if x in imdb_directors]  # match directors' first names
-                    if matches:
-                        return 'tt' + ID
-                sleep(INTERVAL)
-            return None
+        # Search by titles and compares by director's first name
+        dprint('\n>> search_id_by_title')
+        for m in movies[:MOVIES_SEARCH_TOLERANCE]:
+            dprint('movie=', m)
+            ID = m.getID()
+            movie = ia.get_movie(ID)
+            imdb_directors = movie.get('directors')
+            dprint('imdb_directors=', imdb_directors)
+            if imdb_directors:
+                imdb_directors = clean_names(imdb_directors)
+                dprint('cleaned imdb_directors=', imdb_directors)
+                matches = [x for x in cleaned_directors if x in imdb_directors]  # match directors' first names
+                dprint('matched directors=', matches)
+                if matches:
+                    return 'tt' + ID
+            sleep(INTERVAL)
+        return None
+
+    titles = list(titles)
 
     # Parsing entries, if data
-    if data:
-        if isinstance(data, dict):
-            if data.get('directors'):
-                directors = data['directors']
-            if data.get('title_eng'):
-                title_eng = data['title_eng']
-            elif data.get('title'):
-                title_eng = data['title']
-            if data.get('original_title'):
-                original_title = data['original_title']
+    if data and isinstance(data, dict):
+        if data.get('directors'):
+            directors = data['directors']
+        if data.get('title_eng'):
+            titles.append(data['title_eng'])
+        if data.get('title'):
+            titles.append(data['title'])
+        if data.get('original_title'):
+            titles.append(data['original_title'])
 
-    assert directors, '[find_imdb] directors missing'
-    assert title_eng or original_title, '[find_imdb] no title was given'
+    assert directors, '[yellow]\[find_imdb] no director was given'
+    assert titles, '[yellow]\[find_imdb] no title was given'
 
     if isinstance(directors, str):
         directors = [directors]
 
-    # Treating data
-    directors = sorted([clean_name(x, dot=False) for x in directors])[:SEARCH_TOLERANCE]
-    directors_list = directors[:]  # To search_id_by_director()
-    directors = normalize_strings(directors)  # To search_id_by_title() *person' first names
+    # Simplifying data
+    # directors = sorted([clean_string(x) for x in directors])[:SEARCH_TOLERANCE]  # To search_id_by_director()
+    directors = sorted(directors)  # To search_id_by_director()
+    cleaned_directors = clean_names(directors)  # To search_id_by_title() *person' first names
 
-    titles = [x.replace("`", "'") for x in [title_eng, original_title] if x]
+    titles = [x.replace("`", "'") for x in titles if x]  # ` causes search innacuracy
+    cleaned_titles = [clean_title(x) for x in titles]
 
     # Starting
     ia = IMDb()
-    print('[bright_black]\[find_imdb] Searching imdb id...', flush=True)
+    print(f'[bright_black]\[find_imdb] Searching imdb id (titles={" ".join(titles)} | directors={" ".join(directors)} ...')
+
+    dprint('parsed directors=', directors)
+    dprint('cleaned parsed directors=', directors)
+    dprint('parsed titles=', titles)
+    dprint('cleaned parsed titles=', titles)
 
     # Search id by directors
     new_imdb_id = search_id_by_director()
@@ -100,7 +137,8 @@ def main(title_eng=None, original_title=None, directors=None, data:dict=None):
     for title in titles:
         # try `search_movie_advanced` (more accurate, less results)
         try:
-            movies = ia.search_movie_advanced(title)
+            dprint('>> search_movie_advanced')
+            movies = ia.search_movie_advanced(title)  # can raise error
         except (AttributeError, ValueError):
             pass
         else:
@@ -110,20 +148,28 @@ def main(title_eng=None, original_title=None, directors=None, data:dict=None):
             return new_imdb_id
 
         # try regular `search_movie` (less accurate, more results)
+        dprint('>> search_movie')
         movies = ia.search_movie(title)
         new_imdb_id = search_id_by_title(movies)
 
         if new_imdb_id:
             return new_imdb_id
 
-    print(f'[bright_black]\[find_imdb] Not found: {title_eng} ({original_title})', flush=True)
+    print(f'[bright_black]\[find_imdb] Not found: {" ".join(titles)}')
 
 
-def finder(title_eng=None, original_title=None, directors=None, data=None):
-    for _ in range(TRIES):
-        try:
-            new_imdb_id = main(title_eng=title_eng, original_title=original_title, directors=directors, data=data)
-            return new_imdb_id
-        except Exception as e:
-            print(f'[find_imdb] Got Error:[yellow] {e} ')
-    return None
+# def finder(title_eng=None, original_title=None, directors=None, data=None, debug=False):
+def finder(*titles, directors=None, data=None, debug=False):
+    global DEBUG
+    DEBUG = debug
+
+    new_imdb_id = main(*titles, directors=directors, data=data)
+    return new_imdb_id
+
+    # for _ in range(TRIES):
+    #     try:
+    #         new_imdb_id = main(*titles, directors=directors, data=data)
+    #         return new_imdb_id
+    #     except Exception as e:
+    #         print(f'[find_imdb] Got Error:[yellow] {e} ')
+    # return None
